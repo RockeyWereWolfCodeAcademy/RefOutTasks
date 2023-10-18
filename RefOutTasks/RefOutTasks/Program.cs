74a31921aee94a1ff044a09c1e55d3ce5cda2b02
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-18 19:45:38 +0400

    uploaded project

---
+++ RefOutTasks/RefOutTasks/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RefOutTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -5, 10, -15, 20, -25 };
+
+            Console.WriteLine("Original Array:");
+            PrintArray(numbers);
+
+            ConvertNegativesToPositives(ref numbers);
+
+            Console.WriteLine("Modified Array (Negative Elements Converted to Positives):");
+            PrintArray(numbers);
+        }
+
+        static void ConvertNegativesToPositives(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    array[i] = -array[i];
+                }
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.WriteLine(number + " ");
+            }
+        }
+    }
+}
